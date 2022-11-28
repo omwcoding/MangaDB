327e2f09392ef755026cd11631502c6dcb49acdf
omwcoding <116875415+omwcoding@users.noreply.github.com>
2022-11-28 11:13:06 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -5,7 +5,10 @@ INSERT INTO SERIE (nome, anno_pubblicazione, completo) VALUES
 ('Berserk', 1989, 0),
 ('Buonanotte, Punpun', 2007, 1),
 ('Devilman', 1972, 1),
-('One Piece', 1997, 0);
+('Chainsaw Man', 2018, 0);
+('Tatsuki Fujimoto Short Stories', 2018, 0);
+
+
 
 -- Categorie di manga (questi rimarranno fissi)
 INSERT INTO categoria (nome) VALUES 
