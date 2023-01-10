b45203cad1dc943b05a10665258e486c831c962c
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-01-10 23:04:23 +0100

    andre pintux

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,9 +1,9 @@
 -- Active: 1672329880844@@127.0.0.1@3306@mangadb
-CREATE VIEW MangaInPossesso
+CREATE VIEW VolumiInPossesso
 AS
 SELECT nome FROM mangadb.volume WHERE possesso = 1;
 
-CREATE VIEW MangaMancanti
+CREATE VIEW VolumiMancanti
 AS
 SELECT nome FROM mangadb.volume WHERE possesso = 0;
 
@@ -14,4 +14,6 @@ SELECT nome, anno_pubblicazione, stato, n_volumi FROM mangadb.serie ORDER BY `st
 CREATE VIEW Mangaka
 AS
 SELECT nome, nazionalità FROM mangadb.autore;
+
+
 
