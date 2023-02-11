e4a41c5b40e22ac83d0812698509b52778e1403b
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-02-11 10:24:19 +0100

    y

---
+++ Java/com.mangadb/src/com/mangadb/Autore.java
@@ -0,0 +1,13 @@
+package com.mangadb;
+
+public class Autore {
+    
+    String nome;
+    int id_autore;
+
+    Autore(int id_autore, String nome){
+        this.id_autore = id_autore;
+        this.nome = nome;
+    }
+    
+}
---
+++ Java/com.mangadb/src/com/mangadb/Editore.java
@@ -0,0 +1,13 @@
+package com.mangadb;
+
+public class Editore {
+
+    String nome;
+    int id_editore;
+
+    Editore(int id_editore, String nome){
+        this.id_editore = id_editore;
+        this.nome = nome;
+    }
+    
+}
