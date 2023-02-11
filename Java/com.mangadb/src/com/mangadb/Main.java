26736358966ede16dd762defd124c9f75e9ab6e8
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-02-11 10:54:21 +0100

    la hit

--- Java/com.mangadb/src/com/mangadb/Main.java
+++ Java/com.mangadb/src/com/mangadb/Main.java
@@ -2,27 +2,23 @@ package com.mangadb;
 
 public class Main {
 	public static void main(String[] args) {
-		Serie naruto = new Serie(1, "Naruto", false, 3, 0.0, new Volume[3]);
-		Serie berserk = new Serie(2, "Berserk", false, 2, 0.0, new Volume[2]);
-
-		Volume volume1Naruto = new Volume(1, "Naruto volume 1", true, 10.0, naruto);
-		Volume volume2Naruto = new Volume(2, "Naruto volume 2", true, 10.0, naruto);
-		Volume volume3Naruto = new Volume(3, "Naruto volume 3", true, 10.0, naruto);
-
-		Volume volume1Berserk = new Volume(1, "Berserk volume 1", true, 15.0, berserk);
-		Volume volume2Berserk = new Volume(2, "Berserk volume 2", true, 15.0, berserk);
-
-		naruto.volumi[0] = volume1Naruto;
-		naruto.volumi[1] = volume2Naruto;
-		naruto.volumi[2] = volume3Naruto;
-
-		berserk.volumi[0] = volume1Berserk;
-		berserk.volumi[1] = volume2Berserk;
-		
-		double costoNaruto = naruto.calcolaCostoTotale();
+	
+		Serie berserk = new Serie(1, "Berserk", false, 41, 0.0, new Volume[41]);
+		for (int i = 1; i <= 41; i++) {
+			Volume volume = new Volume(i, "Volume " + i, true, 5.5, berserk);
+			berserk.volumi.add(volume);
+		  }
 		double costoBerserk = berserk.calcolaCostoTotale();
+
+		Serie chainsawMan = new Serie(1, "Chainsaw Man", false, 11, 0.0, new Volume[11]);
+		for (int i = 1; i <= 11; i++) {
+			Volume volume = new Volume(i, "Volume " + i, true, 4.9, chainsawMan);
+			chainsawMan.volumi.add(volume);
+		  }
+		double costoChainsawMan = chainsawMan.calcolaCostoTotale();
+
 		
-		System.out.println("Il costo totale di Naruto è: " + costoNaruto);
 		System.out.println("Il costo totale di Berserk è: " + costoBerserk);
+		System.out.println("Il costo totale di Chainsaw Man è: " + costoChainsawMan);
 	}
 }
--- Java/com.mangadb/src/com/mangadb/Serie.java
+++ Java/com.mangadb/src/com/mangadb/Serie.java
@@ -1,4 +1,5 @@
 package com.mangadb;
+import java.util.ArrayList;
 
 public class Serie {
 	int id_serie;
@@ -6,7 +7,7 @@ public class Serie {
 	boolean is_finished;
 	int n_volumi;
 	double costo_totale;
-	Volume[] volumi;
+	ArrayList<Volume> volumi;
 	
 	Serie(int id_serie, String nome, boolean is_finished, int n_volumi, double costo_totale, Volume[] volumi){
 		this.id_serie = id_serie;
@@ -14,7 +15,7 @@ public class Serie {
 		this.is_finished = is_finished;
 		this.n_volumi = n_volumi;
 		this.costo_totale = costo_totale;
-		this.volumi = volumi;
+		this.volumi = new ArrayList<Volume>();
 	}
 
 	public double calcolaCostoTotale() {
