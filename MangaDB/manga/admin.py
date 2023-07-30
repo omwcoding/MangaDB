825927eacee3b9ad8e2cc48a2320a56113ed3780
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-07-30 14:03:08 +0200

    add something

--- MangaDB/manga/admin.py
+++ MangaDB/manga/admin.py
@@ -3,3 +3,7 @@ from .models import Author, Publisher, Collection, MangaVolume, Genre
 # Register your models here.
 
 admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Collection)
+admin.site.register(MangaVolume)
